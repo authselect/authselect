9814741f2fd21a629f65b7852961aa29091c46e5
Pavel Březina <pbrezina@redhat.com>
2018-07-26 15:21:06 +0200

    zanata: create zanata hooks
    Create Zanata hooks to automate translation process. It is expected
    that downloaded *.po files are commited and part of distribution.
    
    Built translations are excluded from distribution as they can
    be computed during compilation.
    
    Resolves:
    https://github.com/pbrezina/authselect/issues/56

---
+++ zanata-hooks.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+zanata_push_before()
+{
+    # Update C pot file
+    make -C "$PWD/po" update-po &> /dev/null
+    if [ $? -ne 0 ]; then
+        echo "Unable to run: make -C \"$PWD/po\" update-po"
+        echo "Make sure that ./configure was run and the project is configured"
+        exit 1
+    fi
+    
+    # Update man pages pot files
+    DIR="$PWD/src/man"
+    for file in $DIR/*.adoc; do
+        name=`basename $file`
+        po4a-gettextize -f asciidoc -m $file -p "$DIR/po/$name.pot" > /dev/null
+        if [ $? -ne 0 ]; then
+            exit 1
+        fi
+    done
+}
+
+zanata_pull_after()
+{
+    # Update LINGUAS file
+    $PWD/linguas.sh $PWD
+}
+
+case $1 in
+push-before)
+  zanata_push_before
+  ;;
+push-after)
+  ;;
+pull-before)
+  ;;
+pull-after)
+  zanata_pull_after
+  ;;
+*)
+  echo "Unknown parameter $1"
+  exit 1
+  ;;
+esac
+
+exit 0
