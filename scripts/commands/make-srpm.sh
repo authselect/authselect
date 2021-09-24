33c1e95b3bb11ec5a9702d774ea9cdfdef8237a7
Pavel Březina <pbrezina@redhat.com>
2021-09-24 16:15:27 +0200

    scripts: add ci automation scripts

---
+++ scripts/commands/make-srpm.sh
@@ -0,0 +1,18 @@
+#
+# Make source RPM.
+#
+
+set -x
+
+builddir=`mktemp -d -p .`
+outdir=`realpath ${1:-$srcdir}`
+trap "rm -fr $builddir" EXIT
+
+autoreconf -if
+pushd "$builddir"
+../configure --enable-silent-rules
+make srpm
+mv rpmbuild/SRPMS/* "$outdir"
+popd
+
+exit 0
