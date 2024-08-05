d5a0d034f02472adb79d0afc36f2682d8efe7e4b
Pavel Březina <pbrezina@redhat.com>
2024-08-05 20:28:42 +0200

    ci: run system tests on pull request and push
    This introduces system tests into our CI. It is modified CI workflow
    taken from SSSD. So far, it is very basic just to have something
    quickly. For the future, I would like to investigate if we can move
    this into Fedora's testing farm.

--- scripts/commands/install-build-deps.sh
+++ scripts/commands/install-build-deps.sh
@@ -11,13 +11,13 @@ fi
 . /etc/os-release
 
 case "$ID" in
-  fedora)
+  fedora | centos | rhel)
     [ ! -f "rpm/authselect.spec.in" ] && die "Missing file: rpm/authselect.spec.in"
     [ -f "authselect.spec" ] && die "./authselect.spec already exist, delete it to continue."
 
     sed -E "s/@\w+@/dummy/g" rpm/authselect.spec.in > authselect.spec
     dnf install -y "dnf-command(builddep)"
-    dnf builddep -y --spec ./authselect.spec
+    dnf builddep -y ./authselect.spec
     rm authselect.spec
     ;;
   ubuntu)
